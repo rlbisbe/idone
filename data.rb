91650a15175c0cf2718a07daa3047b068b5d2c81
Roberto Luis Bisbe <rlbisbe@rlbisbe.local>
2015-07-24 01:31:45 +0200

    added done script

---
+++ data.rb
@@ -0,0 +1,18 @@
+require 'date'
+
+class Done
+	attr_accessor :text, :date
+	def initialize(text, date = Date.today)
+		@text = text
+		@date = date
+	end
+
+	def to_json(options)
+        {'text' => @text, 'date' => @date}.to_json
+    end
+
+    def self.from_json data
+        self.new data['text'], data['date']
+    end
+end
+
